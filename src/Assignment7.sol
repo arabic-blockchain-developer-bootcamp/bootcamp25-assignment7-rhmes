87dde8d47331c3b886c8b0cece23e3538c3618fe
r <eng.raghdaa@gmail.com>
2025-03-13 09:42:55 +0200

    Adder owner modifier

--- src/Assignment7.sol
+++ src/Assignment7.sol
@@ -2,16 +2,20 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Assignment7 is ERC20 {
+contract Assignment7 is ERC20, Ownable {
     // Constructor to initialize the token
     // Fill in the parameters for the token name and symbol
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) Ownable(msg.sender) {}
 
     // Function to mint tokens
     // Fill in the visibility modifier to be external
     // Set params as to address and amount
-    function mint(address owner, uint256 amount) external{
+    function mint(address owner, uint256 amount) external {
         // Fill in the logic
         // call _mint function with to and amount params
         _mint(owner, amount);
